8ecea524c264851605993b64a1671b909a041d7e
harshmehta1999-ux <harsh.mehta@aplyid.com>
2026-03-27 19:12:24 +1300

    Remove default example stories

--- src/stories/DSMButton.stories.ts
+++
@@ -1,8 +0,0 @@
-// Stories have moved to DSMButton.stories.tsx
-import type { Meta } from '@storybook/react-vite';
-import { DSMButton } from './DSMButton';
-
-export default {
-  component: DSMButton,
-  tags: ['!dev', '!autodocs', '!test'],
-} satisfies Meta<typeof DSMButton>;
